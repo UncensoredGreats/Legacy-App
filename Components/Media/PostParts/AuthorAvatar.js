fa5a708a800f05666ec999068421ef8d9cada2dc
evanmcfarland <evanmcfarland.aa@gmail.com>
2023-08-01 11:39:37 -0400

    Image click leads to chatpage on timeless media

--- Components/Media/PostParts/AuthorAvatar.js
+++ Components/Media/PostParts/AuthorAvatar.js
@@ -1,24 +1,5 @@
-// // OG AuthorAvatar.js
-// import React from 'react';
-// import { Grid, Image, Card } from 'semantic-ui-react';
-
-// const AuthorAvatar = ({ post }) => {
-//   return (
-//     <Grid.Column width={6} textAlign="center">
-//       <Image circular src={`/images/${post.author_id}.png`} size="small" style={{ boxShadow: '0 0 10px rgba(0,0,0,0.15)' }} />
-//       <Card.Meta style={{ fontSize: '1em', padding: '0.3em', color: '#555' }}>{post.author_id}</Card.Meta>
-//     </Grid.Column>
-//   )
-// }
-
-// export default AuthorAvatar;
-
-
-
-
-
 // AuthorAvatar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Image, Card } from 'semantic-ui-react';
 
 const timeElapsed = (date) => {
@@ -39,15 +20,40 @@ const timeElapsed = (date) => {
     if (diffInHours > 1) return `${diffInHours} hours ago`;
     if (diffInMinutes > 1) return `${diffInMinutes} minutes ago`;
     return `${diffInSeconds} seconds ago`;
-  }  
+  }
 
-const AuthorAvatar = ({ post }) => {
+const AuthorAvatar = ({ post, onAvatarClick }) => {
   const timeFromNow = timeElapsed(post.created_at);
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const imageStyles = {
+    boxShadow: '0 0 10px rgba(0,0,0,0.15)',
+    cursor: 'pointer',
+    opacity: isHovered ? 0.8 : 1,
+    transform: isHovered ? 'scale(1.05)' : 'scale(1)',
+    transition: 'opacity 0.2s, transform 0.2s'
+  };
 
   return (
     <Grid.Column width={6} textAlign="center">
-        
-      <Image circular src={`/images/${post.author_id}.png`} size="small" style={{ boxShadow: '0 0 10px rgba(0,0,0,0.15)' }} />
+      <Image 
+        circular 
+        src={`/images/${post.author_id}.png`} 
+        size="small" 
+        style={imageStyles}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={onAvatarClick}
+      />
       <Card.Meta style={{ fontSize: '1em', padding: '0.3em', color: '#555' }}>{post.author_id}</Card.Meta>
       <Card.Meta style={{ fontSize: '0.85em', color: '#777' }}>{timeFromNow}</Card.Meta>
     </Grid.Column>
--- Components/Media/PublicAuthorMessage.tsx
+++ Components/Media/PublicAuthorMessage.tsx
@@ -1,12 +1,157 @@
+// import React, { useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { Card, Divider, Grid } from 'semantic-ui-react';
+// import AuthorAvatar from './PostParts/AuthorAvatar';
+// import PostRating from './PostParts/PostRating';
+// import PostContent from './PostParts/PostContent';
+// import { getColorCode } from './PostParts/ColorCode';
+// import { useCardState } from '../../contexts/CardStateContext';
+// import { useActiveChat } from '../../contexts/ActiveChatContext';
+
+// type AuthorAvatarProps = {
+//   post: any; // replace 'any' with an appropriate type if possible
+//   onAvatarClick?: (e: React.MouseEvent) => void; // This line was added
+// };
+
+// const PublicAuthorMessage = ({ post, authorId, isFlipped, flipCard, animationPlayed, setAnimationPlayed }) => {
+//   let userQuestion = "There was an error retrieving the user query.";
+
+//   const { cardState, startChat, stopChat } = useCardState(authorId);
+//   const { activeChat, setActiveChat } = useActiveChat();
+//   const router = useRouter();
+
+//   const handleChatClick = (e: React.MouseEvent) => {
+//     e.preventDefault();
+//     console.log('Chat Clicked');
+//     if (!cardState.chatMode) {
+//       startChat();
+//       setActiveChat(authorId);
+//       router.push(`/chat/${authorId}`);
+//     } else {
+//       stopChat();
+//       setActiveChat(null);
+//       router.push('/');
+//     }
+//   };
+
+  // if (post.metadata) {
+  //   userQuestion = post.user_query;
+  // }
+
+  // let sourceTitle, sourceHeading, sourceContent, sourceSummaries, sourceMetasummary, metadata, messageId;
+  // try {
+  //   sourceTitle = JSON.parse(post.source_title);
+  //   sourceHeading = JSON.parse(post.source_heading);
+  //   sourceContent = JSON.parse(post.source_content);
+  //   sourceSummaries = JSON.parse(post.source_summaries);
+  //   sourceMetasummary = JSON.parse(post.source_metasummary);
+  //   metadata = post.metadata;
+  //   messageId = post.message_id;
+  // } catch (err) {
+  //   console.error('Failed to parse source arrays', err);
+  // }
+
+  // const [showMetaSummary, setShowMetaSummary] = useState(false);
+
+  // const [showMetadata, setShowMetadata] = useState(false);
+  // const toggleMetadata = () => setShowMetadata(!showMetadata);
+
+  // const cardColor = getColorCode(post.prompt_type);
+
+  // const result = {
+  //   cardColor: cardColor,
+  //   jsx: (
+  //     <Grid.Column width={16}>
+  //       <Card.Content>
+  //         <Grid>
+  //           <Grid.Row>
+  //             <AuthorAvatar post={post} onAvatarClick={handleChatClick} />
+  //             <PostRating metadata={metadata} messageId={messageId} toggleMetadata={toggleMetadata} />
+  //           </Grid.Row>
+  //           <Divider />
+  //           <PostContent
+  //             isFlipped={isFlipped}
+  //             flipCard={flipCard}
+  //             userQuestion={userQuestion}
+  //             post={post}
+  //             authorId={post.author_id}
+  //             sourceTitle={sourceTitle}
+  //             sourceHeading={sourceHeading}
+  //             sourceContent={sourceContent}
+  //             sourceSummaries={sourceSummaries}
+  //             sourceMetasummary={sourceMetasummary}
+  //             animationPlayed={animationPlayed}
+  //             setAnimationPlayed={setAnimationPlayed}
+  //             showMetaSummary={showMetaSummary}
+  //             setShowMetaSummary={setShowMetaSummary}
+  //           />
+  //         </Grid>
+  //       </Card.Content>
+  //     </Grid.Column>
+  //   ),
+  // };
+
+//   return result;
+// };
+
+// export default PublicAuthorMessage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import { Card, Divider, Grid } from 'semantic-ui-react';
 import AuthorAvatar from './PostParts/AuthorAvatar';
 import PostRating from './PostParts/PostRating';
 import PostContent from './PostParts/PostContent';
 import { getColorCode } from './PostParts/ColorCode';
+import { useCardState } from '../../contexts/CardStateContext';
+import { useActiveChat } from '../../contexts/ActiveChatContext';
 
-const PublicAuthorMessage = ({ post, authorId, isFlipped, flipCard, animationPlayed, setAnimationPlayed }) => {
+type AuthorAvatarProps = {
+  post: any;
+  onAvatarClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+};
+
+const PublicAuthorMessage = ({ 
+  post, 
+  authorId, 
+  isFlipped, 
+  flipCard, 
+  animationPlayed, 
+  setAnimationPlayed 
+}) => {
   let userQuestion = "There was an error retrieving the user query.";
+
+  const { cardState, startChat, stopChat } = useCardState(authorId);
+  const { activeChat, setActiveChat } = useActiveChat();
+  const router = useRouter();
+
+  const handleChatClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    console.log('Chat Clicked');
+    if (!cardState.chatMode) {
+      startChat();
+      setActiveChat(authorId);
+      router.push(`/chat/${authorId}`);
+    } else {
+      stopChat();
+      setActiveChat(null);
+      router.push('/');
+    }
+  };
+
 
   if (post.metadata) {
     userQuestion = post.user_query;
@@ -39,7 +184,7 @@ const PublicAuthorMessage = ({ post, authorId, isFlipped, flipCard, animationPla
         <Card.Content>
           <Grid>
             <Grid.Row>
-              <AuthorAvatar post={post} />
+              <AuthorAvatar post={post} onAvatarClick={handleChatClick} />
               <PostRating metadata={metadata} messageId={messageId} toggleMetadata={toggleMetadata} />
             </Grid.Row>
             <Divider />
@@ -69,4 +214,3 @@ const PublicAuthorMessage = ({ post, authorId, isFlipped, flipCard, animationPla
 };
 
 export default PublicAuthorMessage;
-
