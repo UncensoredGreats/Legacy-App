57cb1909adacdf770847463fa9615a51aed4e6f6
evanmcfarland <evanmcfarland.aa@gmail.com>
2023-08-08 17:10:29 -0400

    semantic library, multiple requests fix

--- Components/SemanticLibrary/SemanticLibraryPage.tsx
+++ Components/SemanticLibrary/SemanticLibraryPage.tsx
@@ -7,7 +7,7 @@ import { handleReadBookClick } from '../../utils/handleReadBookClick';
 const { Option } = Select;
 const { Title, Text, Paragraph } = Typography;
 
-const defaultAuthor = "Random Corpus";
+const defaultAuthor = "All Books";
 const SemanticLibraryPage = () => {
     const [searchValue, setSearchValue] = useState('');
     const [data, setData] = useState(null);
@@ -51,242 +51,8 @@ const SemanticLibraryPage = () => {
     };
 
     function sanitizeTitleForFilename(title: string): string {
-        return title.replace(/[\/\\\?\*\:\|\<\>\"\.\,\[\]\-]/g, '');
+        return title.replace(/[\/\\\?\*\:\|\<\>\"\.\,\[\]\-\(\)\—]/g, '');
       }
-      
-
-    // return (
-    //     <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '40px' }}>
-    //         <div style={{ textAlign: 'center', marginBottom: '30px' }}>
-    //             <Title level={1}>The Perfect Way to Find Your Next Read</Title>
-    //             <Text type="secondary">Search book sections by semantic meaning. Then, delve deeper as you please!</Text>
-    //         </div>
-    //         <Space align="center" style={{ display: 'flex', justifyContent: 'center', margin: '30px auto' }}>
-    //             <Select defaultValue={defaultAuthor} style={{ width: 120 }} onChange={handleAuthorChange}>
-    //                 <Option value={defaultAuthor}>{defaultAuthor}</Option>
-    //                 {AUTHOR_INFO.map(author => (
-    //                     <Option key={author.id} value={author.id}>{author.id}</Option>
-    //                 ))}
-    //             </Select>
-    //             <Input.Search
-    //                 placeholder="Type a topic or a query..."
-    //                 enterButton={<Button type="primary"><SearchOutlined /></Button>}
-    //                 size="large"
-    //                 onChange={handleSearchChange}
-    //                 onSearch={handleSearchSubmit}
-    //                 style={{ width: '75%' }}
-    //             />
-    //         </Space>
-    //         {isLoading && <Spin size="large" />}
-    //         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: 'calc(100vh - 200px)', position: 'relative' }}>
-    //             {data && data.titles.map((title, index) => {
-    //                 const currentAuthor = data.authors[index];
-    //                 const heading = data.headings[index];
-    //                 const bookImagePath = `/bookimages/${currentAuthor?.id}/${sanitizeTitleForFilename(title)}.png`;
-    //                 const authorImagePath = `/images/${currentAuthor?.id}.png`;
-    //                 console.log(bookImagePath);
-
-    //                 return (
-    //                     <Card style={{ width: '80%', marginTop: '20px' }} key={title}>
-    //                         <Image 
-    //                             src={imageError[title] ? authorImagePath : bookImagePath} 
-    //                             alt={title} 
-    //                             onError={() => handleImageError(title)}
-    //                         />
-    //                         <Divider />
-    //                         <Card.Meta
-    //                             title={<Title level={2}>{title}</Title>}
-    //                             description={<Text type="secondary"><em>{currentAuthor?.id} | Page {heading}</em></Text>}
-    //                         />
-    //                         {!isFlipped ? (
-    //                             <>
-    //                                 <Text strong style={{fontSize: "16px", marginBottom: "10px"}}>Key Sentences: </Text>
-    //                                 <List
-    //                                     size="small"
-    //                                     dataSource={data.summaries[index]}
-    //                                     renderItem={sentence => <List.Item>{(sentence as string)?.trim()}</List.Item>}
-    //                                 />
-    //                             </>
-    //                         ) : (
-    //                             <Paragraph>{data.contents[index]}</Paragraph>
-    //                         )}
-    //                         <Space>
-    //                             <Button type="primary" icon={<BookOutlined />} onClick={() => handleReadBookClick(currentAuthor.id, title)}>Read the whole book</Button>
-    //                             <Button icon={isFlipped ? <RetweetOutlined /> : <RetweetOutlined rotate={180} />} onClick={() => setIsFlipped(!isFlipped)} />
-    //                         </Space>
-    //                     </Card>
-    //                 );
-    //             })}
-    //         </div>
-    //     </div>
-    // );
-
-
-
-
-
-    // return (
-    //     <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '40px' }}>
-    //         <div style={{ textAlign: 'center', marginBottom: '50px' }}>
-    //             <Title level={1}>A Readers Paradise</Title>
-    //             <Text type="secondary">Search book sections by semantic meaning. Then, delve deeper as you please!</Text>
-    //         </div>
-    //         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '40px' }}>
-    //             <Select 
-    //                 placeholder="Choose an author"
-    //                 style={{ width: 200, marginRight: '20px' }} 
-    //                 onChange={handleAuthorChange}
-    //                 defaultValue={defaultAuthor}
-    //             >
-    //                 {AUTHOR_INFO.map(author => (
-    //                     <Option key={author.id} value={author.id}>{author.id}</Option>
-    //                 ))}
-    //             </Select>
-    //             <Input.Search
-    //                 placeholder="Type a topic or a query..."
-    //                 enterButton={<Button type="primary"><SearchOutlined /></Button>}
-    //                 size="large"
-    //                 onChange={handleSearchChange}
-    //                 onSearch={handleSearchSubmit}
-    //                 style={{ width: '60%' }}
-    //             />
-    //         </div>
-    //         {isLoading ? 
-    //             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-    //                 <Spin size="large" />
-    //             </div>
-    //         : 
-    //             data && data.titles.map((title, index) => {
-    //                 const currentAuthor = data.authors[index];
-    //                 const heading = data.headings[index];
-    //                 const bookImagePath = `/bookimages/${currentAuthor?.id}/${sanitizeTitleForFilename(title)}.png`;
-    //                 const authorImagePath = `/images/${currentAuthor?.id}.png`;
-    
-    //                 return (
-    //                     <Card style={{ width: '90%', marginTop: '30px', marginBottom: '30px' }} key={title}>
-    //                         <div onClick={() => handleReadBookClick(currentAuthor.id, title)}>
-    //                             <Image 
-    //                                 src={imageError[title] ? authorImagePath : bookImagePath} 
-    //                                 alt={title} 
-    //                                 onError={() => handleImageError(title)}
-    //                             />
-    //                             <div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white'}}>
-    //                                 Read Full Book
-    //                             </div>
-    //                         </div>
-    //                         <Divider />
-    //                         <Card.Meta
-    //                             title={<Title level={2}>{title}</Title>}
-    //                             description={<Text type="secondary"><em>{currentAuthor?.id} | Page {heading}</em></Text>}
-    //                         />
-    //                         {!isFlipped ? (
-    //                             <>
-    //                                 <Text strong style={{fontSize: "16px", marginBottom: "10px"}}>Key Sentences: </Text>
-    //                                 <List
-    //                                     size="small"
-    //                                     dataSource={data.summaries[index]}
-    //                                     renderItem={sentence => <List.Item>{(sentence as string)?.trim()}</List.Item>}
-    //                                 />
-    //                             </>
-    //                         ) : (
-    //                             <Paragraph>{data.contents[index]}</Paragraph>
-    //                         )}
-    //                         <Space>
-    //                             <Button onClick={() => setIsFlipped(!isFlipped)}>{isFlipped ? 'Show Summary' : 'Show Details'}</Button>
-    //                         </Space>
-    //                     </Card>
-    //                 );
-    //             })
-    //         }
-    //     </div>
-    // );
-
-
-
-
-// // Layed horizontal, but doesn't fit great.
-//     return (
-//         <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
-//             <div style={{ textAlign: 'center', marginBottom: '30px' }}>
-//                 <Title level={1}>A Readers Paradise</Title>
-//                 <Text type="secondary">Search book sections by semantic meaning. Then, delve deeper as you please!</Text>
-//             </div>
-//             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
-//                 <Select 
-//                     placeholder="Choose an author"
-//                     style={{ width: 200, marginRight: '10px' }} 
-//                     onChange={handleAuthorChange}
-//                     defaultValue={defaultAuthor}
-//                 >
-//                     {AUTHOR_INFO.map(author => (
-//                         <Option key={author.id} value={author.id}>{author.id}</Option>
-//                     ))}
-//                 </Select>
-//                 <Input.Search
-//                     placeholder="Type a topic or a query..."
-//                     enterButton={<Button type="primary"><SearchOutlined /></Button>}
-//                     size="large"
-//                     onChange={handleSearchChange}
-//                     onSearch={handleSearchSubmit}
-//                     style={{ width: '60%' }}
-//                 />
-//             </div>
-//             {isLoading ? 
-//                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-//                     <Spin size="large" />
-//                 </div>
-//             : 
-//                 data && data.titles.map((title, index) => {
-//                     const currentAuthor = data.authors[index];
-//                     const heading = data.headings[index];
-//                     const bookImagePath = `/bookimages/${currentAuthor?.id}/${sanitizeTitleForFilename(title)}.png`;
-//                     const authorImagePath = `/images/${currentAuthor?.id}.png`;
-        
-//                     return (
-//                         <div key={title}>
-//                             <Card style={{ width: '90%', marginTop: '20px', marginBottom: '20px' }}>
-//                                 <div style={{ display: 'flex', alignItems: 'center' }}>
-//                                     <Image 
-//                                         src={imageError[title] ? authorImagePath : bookImagePath} 
-//                                         alt={title} 
-//                                         onError={() => handleImageError(title)}
-//                                         width={`calc(20vw + 50px)`}
-//                                         style={{marginRight: '20px', cursor: 'pointer'}}
-//                                         onClick={() => handleReadBookClick(currentAuthor.id, title)}
-//                                     />
-//                                     <div style={{ flexGrow: 1 }}>
-//                                         <Card.Meta
-//                                             title={<Title level={2}>{title}</Title>}
-//                                             description={<Text type="secondary"><em>{currentAuthor?.id} | Page {heading}</em></Text>}
-//                                         />
-//                                         <Text strong style={{fontSize: "16px", marginBottom: "10px"}}>Key Sentences: </Text>
-//                                         <List
-//                                             size="small"
-//                                             dataSource={data.summaries[index]}
-//                                             renderItem={sentence => <List.Item>{(sentence as string)?.trim()}</List.Item>}
-//                                         />
-//                                     </div>
-//                                 </div>
-//                             </Card>
-//                             {isFlipped &&
-//                                 <Card style={{ width: '90%', marginTop: '5px', marginBottom: '20px' }}>
-//                                     <Paragraph>{data.contents[index]}</Paragraph>
-//                                     <Space>
-//                                         <Button onClick={() => setIsFlipped(!isFlipped)}>Hide</Button>
-//                                     </Space>
-//                                 </Card>
-//                             }
-//                             {!isFlipped &&
-//                                 <Space style={{ display: 'block', textAlign: 'right', marginRight: '5%' }}>
-//                                     <Button onClick={() => setIsFlipped(!isFlipped)}>Show Section</Button>
-//                                 </Space>
-//                             }
-//                         </div>
-//                     );
-//                 })
-//             }
-//         </div>
-//     );
 
     function useWindowWidth(): number | undefined {
         const [windowWidth, setWindowWidth] = useState<number | undefined>(undefined);
@@ -318,11 +84,14 @@ const SemanticLibraryPage = () => {
         }
     }
 
+    // import { Select, Input, Button, Card, List, Spin, Image, Typography, Space, Tooltip } from 'antd';
+    // ...
+
     return (
         <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
             <div style={{ textAlign: 'center', marginBottom: '30px' }}>
-                <Title level={1}>A Readers Paradise</Title>
-                <Text type="secondary">Search book sections by semantic meaning. Then, delve deeper as you please!</Text>
+                <Title level={1}>Welcome to the Semantic Library</Title>
+                <Text type="secondary">Enter a meaningful idea, get the most related book sections. Then, delve deeper as you please!</Text>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
                 <Select 
@@ -354,6 +123,7 @@ const SemanticLibraryPage = () => {
                     const heading = data.headings[index];
                     const bookImagePath = `/bookimages/${currentAuthor?.id}/${sanitizeTitleForFilename(title)}.png`;
                     const authorImagePath = `/images/${currentAuthor?.id}.png`;
+                    console.log(bookImagePath);
         
                     return (
                         <Card style={{ width: '90%', marginTop: '20px', marginBottom: '20px' }}>
--- pages/api/semantic-library.tsx
+++ pages/api/semantic-library.tsx
@@ -91,7 +91,105 @@
 
 
 
-// Adding single author select.
+// // Adding single author select.
+// import natural from "natural";
+// import { TfIdf } from "natural";
+// import { bookSearch, extractLibraryData } from '../../utils/weaviate';
+// import AUTHOR_INFO from "../../data/author_data";
+
+// const key = process.env.OPENAI_API_KEY;
+
+// const authorsDescriptions = {};
+
+// for (const author of AUTHOR_INFO) {
+//     authorsDescriptions[author.cluster] = author.description;
+// }
+
+// let tfidf = new TfIdf();
+
+// for (let author in authorsDescriptions) {
+//     tfidf.addDocument(authorsDescriptions[author]);
+// }
+
+// function getMostRelatedAuthors(query, tfidf, authorsDescriptions) {
+//     const similarities = {};
+
+//     tfidf.tfidfs(query, (i, measure) => {
+//         const author = Object.keys(authorsDescriptions)[i];
+//         similarities[author] = measure;
+//     });
+
+//     const sortedAuthors = Object.keys(similarities).sort((a, b) => similarities[b] - similarities[a]);
+//     return sortedAuthors.slice(0, 1);
+// }
+
+
+
+// const defaultAuthor = AUTHOR_INFO[0].cluster;
+
+// export default async function handler(req, res) {
+//     try {
+//         const { query, author: authorId } = req.body; 
+
+//         // Convert the selected author's id back to a cluster
+//         const author = AUTHOR_INFO.find(a => a.id === authorId)?.cluster;
+
+//         const relatedAuthors = author !== defaultAuthor
+//             ? [author]
+//             : getMostRelatedAuthors(query, tfidf, authorsDescriptions);
+
+
+//         let combinedData = {
+//             authors: [],
+//             titles: [],
+//             headings: [],
+//             contents: [],
+//             summaries: [],
+//         };
+
+//         for (const authorCluster of relatedAuthors) {
+//             const authorInfo = AUTHOR_INFO.find(a => a.cluster === authorCluster);
+
+//             if (authorInfo) {
+//                 const sources = await bookSearch(query, 1, authorInfo.cluster, key);
+//                 const data = await extractLibraryData(sources);
+
+//                 combinedData.titles.push(...data.titles);
+//                 combinedData.headings.push(...data.headings);
+//                 combinedData.contents.push(...data.contents);
+//                 combinedData.summaries.push(...data.summaries);
+
+//                 // Add the required fields from authorInfo to combinedData
+//                 combinedData.authors.push({
+//                     id: authorInfo.id,
+//                     first: authorInfo.first,
+//                     last: authorInfo.last,
+//                     image: authorInfo.image,
+//                     description: authorInfo.description,
+//                     category: authorInfo.category,
+//                     cap_first: authorInfo.cap_first,
+//                     cluster: authorInfo.cluster,
+//                     books: authorInfo.books,
+//                     sentences_json: authorInfo.sentences_json,
+//                     segments_json: authorInfo.segments_json,
+//                     paragraphs_json: authorInfo.paragraphs_json,
+//                 });
+//             }
+//         }
+
+//         res.status(200).json(combinedData);
+
+//     } catch (error) {
+//         console.error("Error in semantic-library API endpoint:", error);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// }
+
+
+
+
+
+
 import natural from "natural";
 import { TfIdf } from "natural";
 import { bookSearch, extractLibraryData } from '../../utils/weaviate';
@@ -111,7 +209,7 @@ for (let author in authorsDescriptions) {
     tfidf.addDocument(authorsDescriptions[author]);
 }
 
-function getMostRelatedAuthors(query, tfidf, authorsDescriptions) {
+function getMostRelatedAuthors(query, tfidf, authorsDescriptions, num = 3) {
     const similarities = {};
 
     tfidf.tfidfs(query, (i, measure) => {
@@ -120,24 +218,25 @@ function getMostRelatedAuthors(query, tfidf, authorsDescriptions) {
     });
 
     const sortedAuthors = Object.keys(similarities).sort((a, b) => similarities[b] - similarities[a]);
-    return sortedAuthors.slice(0, 1);
+    return sortedAuthors.slice(0, num);
 }
-
-
-
-const defaultAuthor = AUTHOR_INFO[0].cluster;
 
 export default async function handler(req, res) {
     try {
         const { query, author: authorId } = req.body; 
 
-        // Convert the selected author's id back to a cluster
-        const author = AUTHOR_INFO.find(a => a.id === authorId)?.cluster;
+        let relatedAuthors;
 
-        const relatedAuthors = author !== defaultAuthor
-            ? [author]
-            : getMostRelatedAuthors(query, tfidf, authorsDescriptions);
-
+        if (authorId) {
+            const authorCluster = AUTHOR_INFO.find(a => a.id === authorId)?.cluster;
+            if (authorCluster) {
+                relatedAuthors = [authorCluster];
+            } else {
+                relatedAuthors = getMostRelatedAuthors(query, tfidf, authorsDescriptions);
+            }
+        } else {
+            relatedAuthors = getMostRelatedAuthors(query, tfidf, authorsDescriptions);
+        }
 
         let combinedData = {
             authors: [],
@@ -184,4 +283,3 @@ export default async function handler(req, res) {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
-
