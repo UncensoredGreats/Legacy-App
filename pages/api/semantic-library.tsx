7b7ac476a5a08e6436b98f11b691f0fce5d19123
evanmcfarland <evanmcfarland.aa@gmail.com>
2023-08-11 09:44:45 -0400

    minor ui changes to semantic library.

--- pages/api/semantic-library.tsx
+++ pages/api/semantic-library.tsx
@@ -1,123 +1,3 @@
-// // OG
-
-// import expressRateLimit from "express-rate-limit";
-// import { TfIdf } from "natural";
-// import { bookSearch, extractLibraryData } from '../../utils/weaviate';
-// import AUTHOR_INFO from "../../data/author_data";
-
-// const key = process.env.OPENAI_API_KEY;
-
-// const apiLimiter = expressRateLimit({
-//     windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
-//     max: 40, // limit each IP to 50 requests per windowMs
-//     message: "Too many queries created from this IP, please try again after an hour",
-//     keyGenerator: (req) => {
-//         return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-//     }
-// });
-
-// const authorsDescriptions = {};
-
-// for (const author of AUTHOR_INFO) {
-//     authorsDescriptions[author.cluster] = author.description;
-// }
-
-// let tfidf = new TfIdf();
-
-// for (let author in authorsDescriptions) {
-//     tfidf.addDocument(authorsDescriptions[author]);
-// }
-
-// function getMostRelatedAuthors(query, tfidf, authorsDescriptions, num = 3) {
-//     const similarities = {};
-
-//     tfidf.tfidfs(query, (i, measure) => {
-//         const author = Object.keys(authorsDescriptions)[i];
-//         similarities[author] = measure;
-//     });
-
-//     const sortedAuthors = Object.keys(similarities).sort((a, b) => similarities[b] - similarities[a]);
-//     return sortedAuthors.slice(0, num);
-// }
-
-// export default async function handler(req, res) {
-//     const isRateLimited = await new Promise((resolve) => {
-//         apiLimiter(req, res, (next) => {
-//             if (res.headersSent) resolve(true);
-//             else resolve(false);
-//         });
-//     });
-
-//     if (isRateLimited) {
-//         return res.status(429).json({ error: "Too many requests. Please try again later." });
-//     }    
-
-//     try {
-//         const { query, author: authorId } = req.body; 
-
-//         let relatedAuthors;
-
-//         if (authorId) {
-//             const authorCluster = AUTHOR_INFO.find(a => a.id === authorId)?.cluster;
-//             if (authorCluster) {
-//                 relatedAuthors = [authorCluster];
-//             } else {
-//                 relatedAuthors = getMostRelatedAuthors(query, tfidf, authorsDescriptions);
-//             }
-//         } else {
-//             relatedAuthors = getMostRelatedAuthors(query, tfidf, authorsDescriptions);
-//         }
-
-//         let combinedData = {
-//             authors: [],
-//             titles: [],
-//             headings: [],
-//             contents: [],
-//             summaries: [],
-//         };
-
-//         for (const authorCluster of relatedAuthors) {
-//             const authorInfo = AUTHOR_INFO.find(a => a.cluster === authorCluster);
-
-//             if (authorInfo) {
-//                 const sources = await bookSearch(query, 1, authorInfo.cluster, key);
-//                 const data = await extractLibraryData(sources);
-
-//                 combinedData.titles.push(...data.titles);
-//                 combinedData.headings.push(...data.headings);
-//                 combinedData.contents.push(...data.contents);
-//                 combinedData.summaries.push(...data.summaries);
-
-//                 // Add the required fields from authorInfo to combinedData
-//                 combinedData.authors.push({
-//                     id: authorInfo.id,
-//                     first: authorInfo.first,
-//                     last: authorInfo.last,
-//                     image: authorInfo.image,
-//                     description: authorInfo.description,
-//                     category: authorInfo.category,
-//                     cap_first: authorInfo.cap_first,
-//                     cluster: authorInfo.cluster,
-//                     books: authorInfo.books,
-//                     sentences_json: authorInfo.sentences_json,
-//                     segments_json: authorInfo.segments_json,
-//                     paragraphs_json: authorInfo.paragraphs_json,
-//                 });
-//             }
-//         }
-
-//         res.status(200).json(combinedData);
-
-//     } catch (error) {
-//         console.error("Error in semantic-library API endpoint:", error);
-//         res.status(500).json({ error: "Internal Server Error" });
-//     }
-// }
-
-
-
-
-
 
 import expressRateLimit from "express-rate-limit";
 import { bookSearch, extractLibraryData } from '../../utils/weaviate';
@@ -245,24 +125,10 @@ export default async function handler(req, res) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // First attempt at using the flask version.
+// // OG with IFIDF
 
 // import expressRateLimit from "express-rate-limit";
+// import { TfIdf } from "natural";
 // import { bookSearch, extractLibraryData } from '../../utils/weaviate';
 // import AUTHOR_INFO from "../../data/author_data";
 
@@ -277,6 +143,30 @@ export default async function handler(req, res) {
 //     }
 // });
 
+// const authorsDescriptions = {};
+
+// for (const author of AUTHOR_INFO) {
+//     authorsDescriptions[author.cluster] = author.description;
+// }
+
+// let tfidf = new TfIdf();
+
+// for (let author in authorsDescriptions) {
+//     tfidf.addDocument(authorsDescriptions[author]);
+// }
+
+// function getMostRelatedAuthors(query, tfidf, authorsDescriptions, num = 3) {
+//     const similarities = {};
+
+//     tfidf.tfidfs(query, (i, measure) => {
+//         const author = Object.keys(authorsDescriptions)[i];
+//         similarities[author] = measure;
+//     });
+
+//     const sortedAuthors = Object.keys(similarities).sort((a, b) => similarities[b] - similarities[a]);
+//     return sortedAuthors.slice(0, num);
+// }
+
 // export default async function handler(req, res) {
 //     const isRateLimited = await new Promise((resolve) => {
 //         apiLimiter(req, res, (next) => {
@@ -290,43 +180,20 @@ export default async function handler(req, res) {
 //     }    
 
 //     try {
-//         const { query, author: authorId } = req.body;
+//         const { query, author: authorId } = req.body; 
 
-//         // Extract author IDs and categories from AUTHOR_INFO for new Flask logic
-//         const authorsData = AUTHOR_INFO.map(author => ({
-//             name: author.id,
-//             category: author.category
-//         }));
+//         let relatedAuthors;
 
-
-
-
-
-//         const payload = {
-//             query,
-//             authors: authorsData
-//         };
-//         console.log("Sending payload to Flask:", JSON.stringify(payload, null, 2));  // Pretty print the payload for better readability
-        
-
-
-
-
-
-//         const response = await fetch('https://extractivesummarizer-77f3c3288124.herokuapp.com/related-authors', {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify({
-//                 query,
-//                 authors: authorsData
-//             }),
-//         });
-
-//         const data = await response.json();
-//         const relatedAuthorsFromFlask = data.related_authors;
-
-//         // Modify logic to get author info
-//         let relatedAuthors = authorId ? [AUTHOR_INFO.find(a => a.id === authorId)?.cluster] : relatedAuthorsFromFlask;
+//         if (authorId) {
+//             const authorCluster = AUTHOR_INFO.find(a => a.id === authorId)?.cluster;
+//             if (authorCluster) {
+//                 relatedAuthors = [authorCluster];
+//             } else {
+//                 relatedAuthors = getMostRelatedAuthors(query, tfidf, authorsDescriptions);
+//             }
+//         } else {
+//             relatedAuthors = getMostRelatedAuthors(query, tfidf, authorsDescriptions);
+//         }
 
 //         let combinedData = {
 //             authors: [],
@@ -348,6 +215,7 @@ export default async function handler(req, res) {
 //                 combinedData.contents.push(...data.contents);
 //                 combinedData.summaries.push(...data.summaries);
 
+//                 // Add the required fields from authorInfo to combinedData
 //                 combinedData.authors.push({
 //                     id: authorInfo.id,
 //                     first: authorInfo.first,
@@ -372,3 +240,8 @@ export default async function handler(req, res) {
 //         res.status(500).json({ error: "Internal Server Error" });
 //     }
 // }
+
+
+
+
+
